3322c89d91f30283d988fadefcef357acd4af866
Nicolás Quintana <nicolas.quintana@usach.cl>
2020-01-07 01:35:34 -0300

    avance front checkout

--- src/views/checkout/Create.vue
+++ src/views/checkout/Create.vue
@@ -8,8 +8,8 @@
             <div class="col-xs-12 col-md-2"></div>
             <div class="col-xs-12 col-md-8">
               <div class="form-group">
-                <label for="nationality">Habitación</label>
-                <select class="form-control" name="country" id="country" v-model="formCheckin.country" required>
+                <label for="room">Habitación</label>
+                <select class="form-control" name="room" id="room" v-model="formCheckout.room" required>
                   <option v-for="(item, index) in countries" v-bind:key="index" :value="item.label">{{ item.label }}</option>
                 </select>
               </div>
@@ -20,13 +20,13 @@
             <div class="col-xs-12 col-md-6">
               <div class="form-group">
                 <label for="name">Nombre Completo</label>
-                <input type="text" class="form-control" id="name" v-model="formCheckin.name" :disabled="true" required />
+                <input type="text" class="form-control" id="name" v-model="formCheckout.name" :disabled="true" required />
               </div>
             </div>
             <div class="col-xs-12 col-md-6">
               <div class="form-group">
-                <label for="dni">Noches Alojadas</label>
-                <input type="text" class="form-control" id="dni" v-model="formCheckin.dni" :disabled="true" required />
+                <label for="days">Noches Alojadas</label>
+                <input type="text" class="form-control" id="days" v-model="formCheckout.days" :disabled="true" required />
               </div>
             </div>
 
@@ -36,9 +36,9 @@
 
             <div class="col-xs-12 col-md-6">
               <div class="form-group">
-                <label for="dniType">Forma de Pago</label>
-                <select class="form-control" name="dnisType" id="dnisType" v-model="formCheckin.dniType" required>
-                  <option v-for="(item, index) in dnisType" v-bind:key="index" :value="item">{{ item }}</option>
+                <label for="payment-method">Forma de Pago</label>
+                <select class="form-control" name="payment-method" id="payment-method" v-model="formCheckout.paymentMethod" required>
+                  <option v-for="(item, index) in paymentMethods" v-bind:key="index" :value="item">{{ item.label }}</option>
                 </select>
               </div>
             </div>            
@@ -60,85 +60,74 @@ import axios from "axios";
 export default {
   data() {
     return {
-      countries: [],
-      dnisType: [],
-      formCheckin: {
-        bookingCode: '',
+      rooms: [],
+      paymentMethods: [],
+      formCheckout: {
+        room: '',
         name: '',
-        dni: '',
-        country: 0,
-        dniType: ''
+        days: '',
+        paymentMethod: 0
       }
     };
   },
   mounted() {
-    this.readDNISType();
-    this.getCountries();
+    this.getPaymentMethods();
   },
   methods: {
     persist(bookingCode) {
       localStorage.bookingCode = bookingCode;
     },
-    getCountries() {
+    getPaymentMethods() {
       axios
-        .get(`${process.env.VUE_APP_API_URL}/countries`)
+        .get(`${process.env.VUE_APP_API_URL}/paymentMethods`)
         .then(response => {
-          this.countries = response.data.map((country) => {
+          this.paymentMethods = response.data.map((paymentMethod) => {
             return {
-              id: country.id_country,
-              label: country.name
+              id: paymentMethod.id_payment,
+              label: paymentMethod.name
             }
           });
         })
         .catch(error => {
-          this.countries = [{
-            id: "Chile",
-            label: "Chile"
+          this.paymentMethods = [{
+            id: "Efectivo",
+            label: "Efectivo"
           }];
         });
     },
-    readDNISType() {
-      this.dnisType = [
-        "Rut",
-        "Pasaporte",
-        "Otro DNI"
-      ]
-    },
-    addCheckIn() {
-      if (!this.formCheckin) return;
-      this.checkin.push(this.formCheckin);
+    addCheckout() {
+      if (!this.formCheckout) return;
+      this.checkin.push(this.formCheckout);
       this.formCheckin = {
-        bookingCode: this.formCheckin.bookingCode,
-        dni: this.formCheckin.dni,
-        name: this.formCheckin.name,
-        country: this.formCheckin.country,
-        dniType: this.dniType
+        room: this.formCheckout.room,
+        name: this.formCheckout.name,
+        days: this.formCheckout.days,
+        paymentMethod: this.formCheckout.paymentMethod
       };
-      this.saveCheckin();
+      this.saveCheckout();
     },
-    saveCheckin() {
+    saveCheckout() {
       let parsed = JSON.stringify(this.formCheckin);
-      localStorage.setItem('checkin', parsed);
+      localStorage.setItem('checkout', parsed);
     },
     onSubmit() {
       axios
-        .post(`${process.env.VUE_APP_API_URL}/checkin/add`, {
-          name: this.formCheckin.name,
-          dni: this.formCheckin.dni,
-          dniType: this.formCheckin.dniType,
-          code: this.formCheckin.bookingCode,
-          country: this.formCheckin.country,
+        .post(`${process.env.VUE_APP_API_URL}/checkout/add`, {
+          room: this.formCheckout.room,
+          name: this.formCheckout.name,
+          days: this.formCheckout.days,
+          paymentMethod: this.formCheckout.paymentMethod,
         })
         .then((response) => {
           this.persist();
           const isValidResponse = response.data
 
           if (isValidResponse) {
-            this.addCheckIn();
+            this.addCheckout();
           }
         })
         .catch(error => {
-          console.log("No se pudo realizar el Check-In");
+          console.log("No se pudo realizar el Check-Out");
           console.error(error);
         });
     },
